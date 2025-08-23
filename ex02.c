538494a1bc018381676f1fcf50a0334332df53cc
joaog <2025000292@ifam.edu.br>
2025-08-22 20:57:03 -0400

    SLA

---
+++ ex02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+float n1, n2, subtracao;
+printf("Digite o primeiro numero: ");
+scanf("%f",&n1);
+printf("Digite o segundo numero: ");
+scanf("%f",&n2);
+subtracao = n1 - n2;
+printf("O resultado:%f",subtracao);
+return 0;
+}
---
+++ ex03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+float n1, n2, divisao;
+printf("Digite o dividendo aqui: ");
+scanf("%f",&n1);
+printf("Digite o divisor aqui: ");
+scanf("%f",&n2);
+divisao = n1/n2;
+printf("%.f",divisao);
+return 0;
+}
