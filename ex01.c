4cf26161f04de816544bcb2d099136720d2d7c03
Jotaj <2025000292@ifam.edu.br>
2025-08-25 20:17:02 -0400

    arrumando indentação

--- ex01.c
+++ ex01.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main (){
-int n1, n2, soma;
-printf("Primeiro numero:");
-scanf("%d",&n1);
-printf("Segundo numero:");
-scanf("%d",&n2);
-soma = n1 + n2;
-printf("Resultado:%d",soma);
-return 0;
+  int n1, n2, soma;
+  printf("Primeiro numero:");
+  scanf("%d",&n1);
+  printf("Segundo numero:");
+  scanf("%d",&n2);
+  soma = n1 + n2;
+  printf("Resultado:%d",soma);
+  return 0;
 }
