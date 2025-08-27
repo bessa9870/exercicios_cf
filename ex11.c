8551612327cd7e7d31c59d97bf1c7ffb219331ef
Jotaj <2025000292@ifam.edu.br>
2025-08-27 15:16:20 -0400

    ajustando indentação

--- ex11.c
+++ ex11.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <math.h>
-int main(){
-//Declarando vari·veis
-float diagonal, area;
+  int main(){
+//Declarando vari√°veis
+  float diagonal, area;
 //Comandos de entrada
-printf("Digite a diagonal do quadrado aqui: \n");
-scanf("%f", &diagonal);
-//O c·lculo
-area = (diagonal * diagonal) / 2;
-//SaÌda
-printf("A area de um quadrado calculada pela diagonal seria: %f", area);
-return 0;
+  printf("Digite a diagonal do quadrado aqui: \n");
+  scanf("%f", &diagonal);
+//O c√°lculo
+  area = (diagonal * diagonal) / 2;
+//Sa√≠da
+  printf("A area de um quadrado calculada pela diagonal seria: %f", area);
+  return 0;
 }
+
