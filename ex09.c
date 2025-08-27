e966512eab0180cbcae16f3a1d48afa092776eda
Jotaj <2025000292@ifam.edu.br>
2025-08-27 14:55:39 -0400

    ajustando indentação

--- ex09.c
+++ ex09.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <math.h>
-//Honestamente eu ainda não entendi bem qual é o BO dessa main, mas tamo ae kskksssk
-int main(){
+
+  int main(){
 //Declarando as variaveis.
-int ni;
+  int ni;
 //Como o resultado da raiz de um numero inteiro pode ser decimal, vou usar o double.
-float raiz, potencia, resultado;
-//Aqui começa a brincadeira
-printf("Digite o numero aqui: ");
-scanf("%i", &ni);
+  float raiz, potencia, resultado;
+//Aqui comeÃ§a a brincadeira
+  printf("Digite o numero aqui: ");
+  scanf("%i", &ni);
 //Processamento da raiz
-resultado = sqrt(ni);
+  resultado = sqrt(ni);
 //Raiz
-printf("A raiz quadrada seria: %f\n", resultado);
-//Processamento da potenciação
-resultado = pow(ni, 2);
-//Saída potência
-printf("Elevado ao quadrado: %f", resultado);
-return 0;
+  printf("A raiz quadrada seria: %f\n", resultado);
+//Processamento da potenciaÃ§Ã£o
+  resultado = pow(ni, 2);
+//SaÃ­da potÃªncia
+  printf("Elevado ao quadrado: %f", resultado);
+  return 0;
 }
+
 
 
 
