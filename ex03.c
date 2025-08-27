2f37e830c43c558f61a947ca6eb3547acb68db7d
Jotaj <2025000292@ifam.edu.br>
2025-08-27 18:40:01 -0400

    ajustando identação

--- ex03.c
+++ ex03.c
@@ -2,12 +2,16 @@
 
 int main()
 {
+//Declaração de variáveis
   float n1, n2, divisao;
+//Entrada
   printf("Digite o dividendo aqui: ");
   scanf("%f",&n1);
   printf("Digite o divisor aqui: ");
   scanf("%f",&n2);
+//Processamento
   divisao = n1/n2;
+//Saída
   printf("%.f",divisao);
   return 0;
 }
