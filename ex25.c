785a47fb6dedc28f1a9ab3d433c21a0179e9ebea
Jotaj <2025000292@ifam.edu.br>
2025-08-27 15:29:40 -0400

    ajustando indentação

--- ex25.c
+++ ex25.c
@@ -2,21 +2,22 @@
 #include <math.h>
 
 int main() {
-    //DeclaraÁ„o de vari·veis
+//Declara√ß√£o de vari√°veis
     float p_aplicacao_mensal, i_taxa_percentual, i_taxa_decimal, valor_acumulado;
     int n_meses;
-    //Entrada
+//Entrada
     printf("Digite o valor da aplicacao mensal (P): R$ ");
     scanf("%f", &p_aplicacao_mensal);
     printf("Digite a taxa de juros mensal (%%, ex: 0.5 para 0.5%%): ");
     scanf("%f", &i_taxa_percentual);
     printf("Digite o numero de meses para a aplicacao (n): ");
     scanf("%d", &n_meses);
-    //Processamento
+//Processamento
     i_taxa_decimal = i_taxa_percentual / 100.0f;
     valor_acumulado = (p_aplicacao_mensal * (powf(1 + i_taxa_decimal, n_meses) - 1)) / i_taxa_decimal;
-    //SaÌda
+//Sa√≠da
     printf("O valor acumulado no final do periodo sera de: R$ %.2f\n", valor_acumulado);
 
     return 0;
 }
+
