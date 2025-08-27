5790c57abe7af6633742e00b141c4a8ee93b3f22
Jotaj <2025000292@ifam.edu.br>
2025-08-27 15:24:54 -0400

    ajustando indentação

--- ex17.c
+++ ex17.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    //Declarando vari·veis
+//Declarando vari√°veis
     char nome1[20], nome2[20], nome3[20];
-    //Entrada
+ //Entrada
     printf("Digite o primeiro nome: ");
     scanf("%s", &nome1);
     printf("Digite o segundo nome: ");
     scanf("%s", &nome2);
     printf("Digite o terceiro nome: ");
     scanf("%s", &nome3);
-    //Processamento/SaÌda
+  //Processamento/Sa√≠da
     printf("%s, %s\n",nome1, nome3);
     printf("%s", nome2);
     return 0;
 }
+
