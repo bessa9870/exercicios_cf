eaf37ea6215e8226566ef0cdc4eda5cf2e2d90d6
Jotaj <2025000292@ifam.edu.br>
2025-08-27 15:25:43 -0400

    ajustando indentação

--- ex18.c
+++ ex18.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
 int main(){
-   //Declarando vari·veis
+//Declarando vari√°veis
    float total, quantidade1, valor1, quantidade2, valor2, quantidade3, valor3;
    printf("Digite a quantidade do primeiro produto: ");
    scanf("%f", &quantidade1);
    printf("Digite o valor do primeiro produto: ");
    scanf("%f", &valor1);
-
    printf("Digite a quantidade do segundo produto: ");
    scanf("%f", &quantidade2);
    printf("Digite o valor do segundo produto: ");
    scanf("%f", &valor2);
-
    printf("Digite a quantidade do terceiro produto: ");
    scanf("%f", &quantidade3);
    printf("Digite o valor do terceiro produto: ");
    scanf("%f", &valor3);
 
-   //Processamento
+//Processamento
    total = (quantidade1 * valor1) + (quantidade2 * valor2) + (quantidade3 * valor3);
-   //SaÌda
+//Sa√≠da
    printf("Os valores dos produtos seriam:%f", total);
     return 0;
 }
+
 
