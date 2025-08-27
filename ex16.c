c0a6b972a7c945b979a1cfb1e2eb56f9a7202826
Jotaj <2025000292@ifam.edu.br>
2025-08-27 15:24:20 -0400

    ajustando indentação

--- ex16.c
+++ ex16.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
 int main(){
-int ano_nasc, idade, ano_atual;
-float idade_se;
-printf("Digite o ano que voce nasceu :");
-scanf("%i",&ano_nasc);
-printf("Digite o ano atual:");
-scanf("%i", &ano_atual);
-idade = ano_atual - ano_nasc;
-printf("Sua idade seria:%i\n", idade);
-idade_se = 365 / 7 * idade;
-printf("Sua idade em semanas: %f", idade_se);
-return 0;
+//Declarando variávies
+  int ano_nasc, idade, ano_atual;
+  float idade_se;
+//Entrada
+  printf("Digite o ano que voce nasceu :");
+  scanf("%i",&ano_nasc);
+  printf("Digite o ano atual:");
+  scanf("%i", &ano_atual);
+//Processamento
+  idade = ano_atual - ano_nasc;
+//Saída
+  printf("Sua idade seria:%i\n", idade);
+//Processamento dnv
+  idade_se = 365 / 7 * idade;
+//Saída dnv
+  printf("Sua idade em semanas: %f", idade_se);
+  return 0;
 }
+
 
 
