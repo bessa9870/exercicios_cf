f870a32c60c651e1221785c2e5b11d8977a82908
Jotaj <2025000292@ifam.edu.br>
2025-08-27 15:22:43 -0400

    ajustando indentação

--- ex15.c
+++ ex15.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
 int main(){
-int peso;
-//Declarando vari�vel
-float pesog, resultado;
+  int peso;
+//Declarando variável
+  float pesog, resultado;
 //Comandos de entrada
-printf("Digite seu peso. Use apemas numeros inteiros: \n");
-scanf("%i", &peso);
-pesog = peso * 1000;
-printf("Seu peso em gramas seria: %.2f\n", pesog);
-resultado = pesog * 5/100 + pesog;
-printf("Seu peso mais 5%%: %f", resultado);
-return 0;
+  printf("Digite seu peso. Use apemas numeros inteiros: \n");
+  scanf("%i", &peso);
+//Processamento
+  pesog = peso * 1000;
+//Saída 
+  printf("Seu peso em gramas seria: %.2f\n", pesog);
+//Processamento dnv
+  resultado = pesog * 5/100 + pesog;
+//Saída dnv
+  printf("Seu peso mais 5%%: %f", resultado);
+  return 0;
 }
+
 
 
