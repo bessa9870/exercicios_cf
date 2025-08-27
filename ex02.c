dd870383c9f1341603eca2d1db1204b8b6193c22
Jotaj <2025000292@ifam.edu.br>
2025-08-27 18:38:34 -0400

    ajustando identação

--- ex02.c
+++ ex02.c
@@ -2,12 +2,16 @@
 
 int main()
 {
+//Declaração de variáveis
   float n1, n2, subtracao;
+//Entrada
   printf("Digite o primeiro numero: ");
   scanf("%f",&n1);
   printf("Digite o segundo numero: ");
   scanf("%f",&n2);
+//Processamento
   subtracao = n1 - n2;
+//Saída
   printf("O resultado:%f",subtracao);
   return 0;
 }
