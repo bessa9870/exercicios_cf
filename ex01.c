f740183b360ec29266225e8fabdde638388ca2e8
Jotaj <2025000292@ifam.edu.br>
2025-08-27 18:37:07 -0400

    ajustando identação

--- ex01.c
+++ ex01.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
 int main (){
+//Declaração de variáveis
   int n1, n2, soma;
+//Entrada
   printf("Primeiro numero:");
   scanf("%d",&n1);
   printf("Segundo numero:");
   scanf("%d",&n2);
+//Processamento
   soma = n1 + n2;
+//Saída
   printf("Resultado:%d",soma);
   return 0;
 }
