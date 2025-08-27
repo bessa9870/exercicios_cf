a339aa5898df7f3991f00cbe1ac771420b62cb38
Jotaj <2025000292@ifam.edu.br>
2025-08-27 15:30:11 -0400

    ajustando indentação

--- ex26.c
+++ ex26.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    //DeclaraÁ„o de vari·veis
+//Declara√ß√£o de vari√°veis
     float distancia_km, velocidade_kmh, tempo_decimal, velocidade_ms;
     int horas, minutos;
-    //Entrada
+//Entrada
     printf("Digite a distancia entre os dois pontos (em KM): ");
     scanf("%f", &distancia_km);
     printf("Digite a velocidade media do percurso (em km/h): ");
     scanf("%f", &velocidade_kmh);
-    //Processamento
+//Processamento
     tempo_decimal = distancia_km / velocidade_kmh;
     horas = (int)tempo_decimal;
     minutos = (int)((tempo_decimal - horas) * 60);
     velocidade_ms = velocidade_kmh / 3.6f;
-    //SaÌda
+//Sa√≠da
     printf("O tempo medio de chegada sera de: %d hora(s) e %d minuto(s).\n", horas, minutos);
     printf("A velocidade de %.2f km/h equivale a %.2f m/s.\n", velocidade_kmh, velocidade_ms);
 
     return 0;
 }
+
