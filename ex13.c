8d17f16e3535135da341cac434978860cf27ce7b
Jotaj <2025000292@ifam.edu.br>
2025-08-27 15:19:43 -0400

    ajustando indentação

--- ex13.c
+++ ex13.c
@@ -1,10 +1,14 @@
 #include <math.h>
 
 int main(){
-float salario, valor_acrescimo;
-printf("Salario aqui: ");
-scanf("%f", &salario);
-valor_acrescimo = (salario * 0.25) + salario;
-printf("Seu salario com acresscimo seria: %.2f", valor_acrescimo);
-return 0;
+//Declarando Variáveis
+  float salario, valor_acrescimo;
+  printf("Salario aqui: ");
+  scanf("%f", &salario);
+//Processamento
+  valor_acrescimo = (salario * 0.25) + salario;
+//Saída
+  printf("Seu salario com acresscimo seria: %.2f", valor_acrescimo);
+  return 0;
 }
+
