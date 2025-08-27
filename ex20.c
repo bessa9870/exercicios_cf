01a43fd3d6e928ddda6764e511395333d9a36596
Jotaj <2025000292@ifam.edu.br>
2025-08-27 15:27:02 -0400

    ajustando indentação

--- ex20.c
+++ ex20.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
 int main() {
-    //DeclaraÁ„o de vari·veis
+//Declara√ß√£o de vari√°veis
     int valor1, valor2, valor3;
     int somatorio;
     float media;
-    //Entrada
+//Entrada
     printf("Insira o primeiro valor inteiro (diferente de zero): ");
     scanf("%d", &valor1);
     printf("Insira o segundo valor inteiro (diferente de zero): ");
     scanf("%d", &valor2);
     printf("Insira o terceiro valor inteiro (diferente de zero): ");
     scanf("%d", &valor3);
-    //Processamento
+//Processamento
     somatorio = valor1 + valor2 + valor3;
     media = (float)somatorio / 3; //Pesquisei aqui e, aparentemente, isso transforma temporariamente int em float
-    //SaÌda
-    printf("O somatÛrio dos valores seria: %d\n", somatorio);
-    printf("A mÈdia entre os valores seria: %.2f\n", media);
-    printf("\nResto da divis„o do somatÛrio por cada valor:\n");
+//Sa√≠da
+    printf("O somat√≥rio dos valores seria: %d\n", somatorio);
+    printf("A m√©dia entre os valores seria: %.2f\n", media);
+    printf("\nResto da divis√£o do somat√≥rio por cada valor:\n");
     printf("Resto por %d = %d\n", valor1, somatorio % valor1);
     printf("Resto por %d = %d\n", valor2, somatorio % valor2);
     printf("Resto por %d = %d\n", valor3, somatorio % valor3);
     return 0;
 }
+
